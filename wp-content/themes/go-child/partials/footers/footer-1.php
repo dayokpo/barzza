bb66d0ce65da370d3d4d92a623701fee40852c22
Cedrick Lacuarin <cedrick.lacuarin@gmail.com>
2026-03-05 21:05:06 +0800

    Footer

---
+++ wp-content/themes/go-child/partials/footers/footer-1.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Footer #1
+ *
+ * @package Go
+ */
+
+$has_background = Go\has_footer_background();
+?>
+
+<footer id="footer" class="site-footer">
+                <div class="footer-wrap container">
+                    <div class="footer-inner">
+                        <div class="footer-navigations">
+                                                            <div class="footer-nav">
+                                    <nav id="footer-navigation" aria-label="Footer Menu">
+                                        <ul class="footer-navigation-list">
+                                            <li id="menu-item-26" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-8 current_page_item menu-item-26"><a href="https://barzza.ae/" aria-current="page"><span>Home</span></a></li>
+<li id="menu-item-401" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-401"><a href="https://barzza.ae/#our-houses" aria-current="page"><span>Our houses</span></a></li>
+<li id="menu-item-400" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-400"><a href="https://barzza.ae/#how-it-works" aria-current="page"><span>How it works</span></a></li>
+<li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="https://barzza.ae/partnership/"><span>Partnership</span></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            
+                                                            <div class="social-nav">
+                                    <nav id="social-navigation" aria-label="Social Menu">
+                                        <ul class="footer-navigation-list">
+                                            <li id="menu-item-30" class="instagram menu-item menu-item-type-custom menu-item-object-custom menu-item-30"><a target="_blank" href="https://www.instagram.com/saga.houses"><span>Instagram</span></a></li>
+<li id="menu-item-31" class="youtube menu-item menu-item-type-custom menu-item-object-custom menu-item-31"><a target="_blank" href="https://www.youtube.com/@SAGAHouses"><span>YouTube</span></a></li>
+<li id="menu-item-32" class="linkedin menu-item menu-item-type-custom menu-item-object-custom menu-item-32"><a target="_blank" href="https://www.linkedin.com/company/sagahouses"><span>LinkedIn</span></a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            
+                            <div class="footer-branding">
+                                <div class="footer-copyright">
+                                    <span>©</span>
+                                    <span>2026</span>
+                                    <span>BARZZA.</span>
+                                </div>
+
+                                                                    <div class="copy-nav">
+                                        <nav id="copy-navigation" aria-label="Copyright Menu">
+                                            <ul class="footer-navigation-list">
+                                                <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-24"><a rel="privacy-policy" href="https://barzza.ae/privacy-policy/"><span>Privacy policy</span></a></li>
+<li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="https://barzza.ae/terms-of-service/"><span>Terms of service</span></a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                                            </div>
+                        </div>
+
+                        <div class="footer-contacts">
+                            <div class="footer-contacts-title">Have questions?</div>
+                            <div class="footer-contacts-button">
+                                <a href="/contacts" class="b2 book_consultation">
+                                    <span>Contact us</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
