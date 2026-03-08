d65a15eacc212a33b5d00f0b25aa0fdbd95d9200
Cedrick Lacuarin <cedrick.lacuarin@gmail.com>
2026-03-09 01:41:51 +0800

    Style

--- wp-content/plugins/barzza-custom-tabs/barzza-custom-tabs.php
+++ wp-content/plugins/barzza-custom-tabs/barzza-custom-tabs.php
@@ -18,45 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Trim .description blocks and append a "See more" link when needed.
- * Outputs both full and trimmed content; CSS controls visibility based on screen width.
- *
- * @param string $content Post content HTML.
- * @param int    $word_limit Number of words to keep in excerpt.
- * @param string $post_url Post permalink for the "See more" link.
- * @return string
- */
-function barzza_trim_description_blocks( $content, $word_limit = 10, $post_url = '' ) {
-	$pattern = '#<div\b([^>]*\bclass=(["\'])[^"\']*\bdescription\b[^"\']*\2[^>]*)>(.*?)</div>#is';
-
-	return preg_replace_callback(
-		$pattern,
-		function( $matches ) use ( $word_limit, $post_url ) {
-			$inner_html = trim( $matches[3] );
-			$plain_text = trim( wp_strip_all_tags( $inner_html ) );
-
-			if ( '' === $plain_text ) {
-				return $matches[0];
-			}
-
-			$excerpt = wp_trim_words( $plain_text, (int) $word_limit, '...' );
-
-			// If no trimming needed or no URL, return original
-			if ( $excerpt === $plain_text || '' === $post_url ) {
-				return $matches[0];
-			}
-
-			// Output both full and trimmed versions; CSS will handle responsive display
-			return '<div' . $matches[1] . '>'
-				. '<span class="barzza-description-full">' . $inner_html . '</span>'
-				. '<span class="barzza-description-excerpt">' . esc_html( $excerpt ) . ' <a href="' . esc_url( $post_url ) . '" class="barzza-see-more">' . esc_html__( 'See more', 'barzza-custom-tabs' ) . '</a></span>'
-				. '</div>';
-		},
-		$content
-	);
-}
-
-/**
  * Shortcode: [barzza_tabs]
  * Usage: Place this shortcode where you want the tabs to appear
  * Default queries posts from 'standalone' category
@@ -120,7 +81,6 @@ function barzza_render_tabs_shortcode( $atts ) {
 				>
 					<?php 
 					$content = $tab->post_content;
-					$content = barzza_trim_description_blocks( $content, 10, get_permalink( $tab->ID ) );
 					$tag_list = get_the_term_list( $tab->ID, 'post_tag', '<span class="barzza-tag"></span>' );
 					$tags_html = $tag_list ? '<div class="barzza-post-tags">' . $tag_list . '</div>' : '';
 					$replacement = '<h3 class="barzza-tab-title">' . esc_html( $tab->post_title ) . '</h3>' . $tags_html . '<div class="content';
@@ -150,7 +110,7 @@ function barzza_enqueue_tabs_assets() {
 		'barzza-tabs-style',
 		plugin_dir_url( __FILE__ ) . 'assets/css/tabs.css',
 		array(),
-		'1.0.1'
+		'1.0.2'
 	);
 
 	wp_enqueue_script(
